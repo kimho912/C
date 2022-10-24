5bae239f2e779e46761444e5614616cc506e32a9
Hyunho Kim <69750502+kimho912@users.noreply.github.com>
2022-10-24 01:35:32 -0500

    Delete Makefiles directory

--- Makefiles/hw6.h
+++
@@ -1,8 +0,0 @@
-// Hyun ho Kim 1001967176
-#include "stack_queue6.h"
-#define CAPACITY 8
-
-int jar(float* stack);
-void pop_jar(float* stack,int top);
-void read_info(QUEUE* queue,char *f);
-void search(QUEUE* queue);
